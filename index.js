875691765a09fe24c268c5da319569a8c5d7328a
Fridtjof August Barfod <fridtjof.barfod@innofactor.com>
2024-02-11 21:03:38 +0100

    ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 import getStarfield from "./src/getStarfield.js";
 import { getFresnelMat } from "./src/getFresnelMat.js";
@@ -8,13 +8,13 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 3;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
 const earthGroup = new THREE.Group();
-earthGroup.rotation.z = -23.4 * Math.PI / 180;
+earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
 scene.add(earthGroup);
 new OrbitControls(camera, renderer.domElement);
 const detail = 12;
@@ -41,7 +41,7 @@ const cloudsMat = new THREE.MeshStandardMaterial({
   transparent: true,
   opacity: 0.8,
   blending: THREE.AdditiveBlending,
-  alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'),
+  alphaMap: loader.load("./textures/05_earthcloudmaptrans.jpg"),
   // alphaTest: 0.3,
 });
 const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
@@ -53,7 +53,7 @@ const glowMesh = new THREE.Mesh(geometry, fresnelMat);
 glowMesh.scale.setScalar(1.01);
 earthGroup.add(glowMesh);
 
-const stars = getStarfield({numStars: 2000});
+const stars = getStarfield({ numStars: 2000 });
 scene.add(stars);
 
 const sunLight = new THREE.DirectionalLight(0xffffff);
@@ -63,19 +63,19 @@ scene.add(sunLight);
 function animate() {
   requestAnimationFrame(animate);
 
-  earthMesh.rotation.y += 0.002;
-  lightsMesh.rotation.y += 0.002;
+  // earthMesh.rotation.y += 0.002;
+  // lightsMesh.rotation.y += 0.002;
   cloudsMesh.rotation.y += 0.0023;
-  glowMesh.rotation.y += 0.002;
+  // glowMesh.rotation.y += 0.002;
   stars.rotation.y -= 0.0002;
   renderer.render(scene, camera);
 }
 
 animate();
 
-function handleWindowResize () {
+function handleWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-window.addEventListener('resize', handleWindowResize, false);
+window.addEventListener("resize", handleWindowResize, false);
